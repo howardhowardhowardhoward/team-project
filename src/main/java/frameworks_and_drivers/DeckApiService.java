c4fb02e67ae682cbf21794363fe68a6623bf6a69
Johnson Qin <johnsonqin0@gmail.com>
2025-11-10 18:37:05 -0500

    Deck entity + DeckApiService class

---
+++ src/main/java/frameworks_and_drivers/DeckApiService.java
@@ -0,0 +1,59 @@
+package frameworks_and_drivers;
+
+import entities.Card;
+import okhttp3.*;
+import org.json.*;
+
+import java.io.IOException;
+
+public class DeckApiService {
+    private final String API_URL = "https://deckofcardsapi.com/api/deck";
+    private final OkHttpClient client = new OkHttpClient();
+    String deckId;
+
+    public DeckApiService() throws IOException {
+        Request request = new Request.Builder()
+                .url(API_URL + "/new/shuffle/?deck_count=6")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        JSONObject json = new JSONObject(response.body().string());
+        deckId = json.getString("deck_id");
+    }
+
+    public void shuffleDeck() {
+        try {
+            Request request = new Request.Builder()
+                    .url(API_URL + "/" + deckId + "/shuffle/")
+                    .build();
+            client.newCall(request).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Card drawCard() {
+        try {
+            Request request = new Request.Builder()
+                    .url(API_URL + "/" + deckId + "/draw/?count=1")
+                    .build();
+            Response response = client.newCall(request).execute();
+            JSONObject json = new JSONObject(response.body().string());
+            JSONArray cardArray = json.getJSONArray("cards");
+            JSONObject cardJson = cardArray.getJSONObject(0);
+
+            String value = cardJson.getString("value");
+            int valueInt = Integer.parseInt(value);
+            String suit = cardJson.getString("suit");
+            String code = cardJson.getString("code");
+            String image = cardJson.getString("image");
+
+            return new Card(code, suit, image, valueInt);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
