b92a6b66408188530ff15981abc38c329e074063
howardzhang <zhanghoward2016@gmail.com>
2025-11-17 17:40:00 -0500

    edit player class

--- src/main/java/entities/Player.java
+++ src/main/java/entities/Player.java
@@ -4,20 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-
-    private final String playerId;
-    private String username;
-
     private double balance;
-
     private final List<Hand> hands;
 
 
     public Player(String playerId, String username, double initialBalance) {
-        this.playerId = playerId;
-        this.username = username;
         this.balance = initialBalance;
-
         this.hands = new ArrayList<>();
     }
 
@@ -32,19 +24,6 @@ public class Player {
 
     public void clearHands() {
         this.hands.clear();
-    }
-
-
-    public String getPlayerId() {
-        return playerId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public double getBalance() {
