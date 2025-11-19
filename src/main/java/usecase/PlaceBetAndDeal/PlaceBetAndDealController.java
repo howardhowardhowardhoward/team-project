6e90e32f9aef29dd701d3452cdfd70e585868b80
Johnson Qin <johnsonqin0@gmail.com>
2025-11-18 19:18:38 -0500

    Refine + update clean architecture for Use Case 2.

--- src/main/java/usecase/PlaceBetAndDeal/PlaceBetAndDealController.java
+++ src/main/java/usecase/PlaceBetAndDeal/PlaceBetAndDealController.java
@@ -1,20 +1,14 @@
 package usecase.PlaceBetAndDeal;
 
-import entities.Bet;
-
 public class PlaceBetAndDealController {
-    private PlaceBetAndDealInteractor interactor;
+    private final PlaceBetAndDealInputBoundary interactor;
 
     public PlaceBetAndDealController(PlaceBetAndDealInteractor interactor) {
         this.interactor = interactor;
     }
 
-    public void placeBetAndDeal(double amount) {
-        try {
-            Bet bet = interactor.execute(amount);
-            System.out.println("Bet placed: $" + bet.getAmount());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Invalid bet: " + e.getMessage());
-        }
+    public void execute(double betAmount) {
+        PlaceBetAndDealInputData inputData = new PlaceBetAndDealInputData(betAmount);
+        interactor.execute(inputData);
     }
 }
