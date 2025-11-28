1633f45aa50b522668996c4dd0aab4c5a692cf82
Johnson Qin <johnsonqin0@gmail.com>
2025-11-27 19:46:34 -0500

    Updating GUI + some code refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,35 @@
+import entities.Deck;
+import entities.Game;
+import frameworks_and_drivers.DeckApiService;
+import frameworks_and_drivers.StartGame.StartGameDataAccess;
+import frameworks_and_drivers.LoadGameDataAccess;
+import frameworks_and_drivers.Homepage;
+import interface_adapters.*;
+import interface_adapters.StartGame.StartGameController;
+import interface_adapters.StartGame.StartGamePresenter;
+import interface_adapters.StartGame.StartGameViewModel;
+import usecase.StartGame.*;
+import usecase.LoadGame.*;
+
 public class Main {
+    public static void main(String[] args) {
+        StartGameViewModel startGameViewModel = new StartGameViewModel();
+        StartGamePresenter startGamePresenter = new StartGamePresenter(startGameViewModel);
+        DeckApiService api = new DeckApiService();
+        Deck deck = new Deck(api);
+        Game game = new Game(deck);
+        StartGameDataAccess startGameDataAccess = new StartGameDataAccess(deck, game);
+        StartGameInteractor startGameInteractor = new StartGameInteractor(startGamePresenter,
+                startGameDataAccess);
+        StartGameController startGameController = new StartGameController(startGameInteractor);
+        LoadGameViewModel loadGameViewModel = new LoadGameViewModel();
+        LoadGamePresenter loadGamePresenter = new LoadGamePresenter(loadGameViewModel);
+        LoadGameDataAccess jsonPlayerDataAccess = new LoadGameDataAccess();
+        LoadGameInteractor loadGameInteractor = new LoadGameInteractor(jsonPlayerDataAccess,
+                startGameDataAccess, loadGamePresenter);
+        LoadGameController loadGameController = new LoadGameController(loadGameInteractor);
+
+        new Homepage(startGameController, startGameViewModel, loadGameController, loadGameViewModel,
+                jsonPlayerDataAccess);
+    }
 }
---
+++ src/main/java/frameworks_and_drivers/Homepage.java
@@ -0,0 +1,125 @@
+package frameworks_and_drivers;
+
+import javax.swing.*;
+import java.awt.*;
+
+import interface_adapters.*;
+import interface_adapters.StartGame.StartGameController;
+import interface_adapters.StartGame.StartGameViewModel;
+import usecase.PlaceBetAndDeal.*;
+import usecase.LoadGame.*;
+import entities.*;
+
+public class Homepage extends JFrame {
+    private final StartGameController startGameController;
+    private final StartGameViewModel startGameViewModel;
+    private final LoadGameController loadGameController;
+    private final LoadGameViewModel loadGameViewModel;
+    private final LoadGameDataAccessInterface loadGameDataAccess;
+
+    public Homepage(StartGameController startGameController, StartGameViewModel startGameViewModel,
+                    LoadGameController loadGameController, LoadGameViewModel loadGameViewModel,
+                    LoadGameDataAccessInterface loadGameDataAccess) {
+        this.startGameController = startGameController;
+        this.startGameViewModel = startGameViewModel;
+        this.loadGameController = loadGameController;
+        this.loadGameViewModel = loadGameViewModel;
+        this.loadGameDataAccess = loadGameDataAccess;
+
+        setTitle("Homepage");
+        setSize(1200, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel backgroundPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                GradientPaint gp = new GradientPaint(0, 0, new Color(0, 50, 0)
+                        ,0, getHeight(), new Color(0, 100, 0));
+                g2d.setPaint(gp);
+                g2d.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
+        backgroundPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+
+
+        // Blackjack logo
+        ImageIcon blackjackIcon = new ImageIcon("BlackjackLogoDesign.png");
+        Image scaledImage = blackjackIcon.getImage().getScaledInstance(600, 300
+                , Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        JLabel imageLabel = new JLabel(scaledIcon);
+        imageLabel.setHorizontalAlignment(JLabel.CENTER);
+        backgroundPanel.add(imageLabel, c);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setOpaque(false);
+        buttonPanel.setLayout(new GridLayout(1, 2, 10, 10));
+
+        // Start button
+        JButton startButton = new JButton("Start New Game");
+        buttonPanel.add(startButton);
+        startButton.addActionListener(e -> startNewGame());
+
+        // Load saved game button
+        JButton loadButton = new JButton("Load Saved Game");
+        buttonPanel.add(loadButton);
+        loadButton.addActionListener(e -> loadSavedGame());
+
+        c.gridy = 5;
+        backgroundPanel.add(buttonPanel, c);
+
+        add(backgroundPanel);
+
+        loadGameViewModel.addPropertyChangeListener(e -> {
+            switch (e.getPropertyName()) {
+                case "message" -> JOptionPane.showMessageDialog(this, loadGameViewModel.getMessage());
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private void startNewGame() {
+        // Overwrite the current saved game with starting balance = 1000
+        loadGameDataAccess.saveBalance(1000);
+
+        startGameController.startGame();
+        PlaceBetAndDealViewModel betViewModel = new PlaceBetAndDealViewModel();
+        PlaceBetAndDealPresenter betPresenter = new PlaceBetAndDealPresenter(betViewModel);
+        Deck deck = startGameController.getDeck();
+        Player player = startGameController.getPlayer();
+        Dealer dealer = startGameController.getDealer();
+        PlaceBetAndDealDataAccess betDataAccess = new PlaceBetAndDealDataAccess(player, deck, dealer);
+        PlaceBetAndDealInteractor betInteractor = new PlaceBetAndDealInteractor(
+                betDataAccess, betPresenter
+        );
+        PlaceBetAndDealController betController = new PlaceBetAndDealController(betInteractor);
+        betPresenter.presentBetUpdated(player.getBalance(), 0);
+        dispose();
+        new BetScreen(betController, betViewModel);
+    }
+
+    private void loadSavedGame() {
+        Game savedGame = loadGameController.loadGame();
+
+        PlaceBetAndDealViewModel betViewModel = new PlaceBetAndDealViewModel();
+        PlaceBetAndDealPresenter betPresenter = new PlaceBetAndDealPresenter(betViewModel);
+        Deck deck = savedGame.getDeck();
+        Player player = savedGame.getPlayer();
+        Dealer dealer = savedGame.getDealer();
+        PlaceBetAndDealDataAccess betDataAccess = new PlaceBetAndDealDataAccess(player, deck, dealer);
+        PlaceBetAndDealInteractor betInteractor = new PlaceBetAndDealInteractor(
+                betDataAccess, betPresenter
+        );
+        PlaceBetAndDealController betController = new PlaceBetAndDealController(betInteractor);
+        betPresenter.presentBetUpdated(player.getBalance(), 0);
+        dispose();
+        new BetScreen(betController, betViewModel);
+    }
+}
